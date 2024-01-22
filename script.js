4f894d7082c0006d4d815155f3d4a27018fabb26
Devang Bondre <shinigamiryuk389@gmail.com>
2024-01-22 14:14:35 +0530

    code files has been updated

--- script.js
+++ script.js
@@ -8,7 +8,8 @@ let name = document.getElementById("name")
 let password = document.getElementById("password")
 let dob = document.getElementById("dob")
 let email = document.getElementById("email")
-
+let phoneno = document.getElementById("phoneNo")
+let popup =document.getElementById("popup") 
 
    if(name.value ===""|| password.value === "" || dob.value ==="" || email.value === ""){
        alert("Please fill the required fields")
@@ -17,5 +18,12 @@ let email = document.getElementById("email")
 
    }    
 
+   if(name.value !== "" && password.value !== "" && dob.value !== "" && email.value !== "" && phoneno.value !== ""  ){
+    // popup.style.transform = 'translate(-50%, -50%) scale(1)';
+    popup.style.visibility = 'visible';
+   }
+   else{
+
+   }
 }
 
